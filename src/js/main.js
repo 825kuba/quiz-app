ced20f3ea4ac4be186a5796cc9687e5b236e4243
825kuba <825kuba@gmail.com>
2021-12-19 10:22:07 +1300

    Finished basic functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const container = document.querySelector('.container');
+
 const settingsEle = document.querySelector('.settings');
 const settingsCategories = document.querySelector('.settings__category');
 const settingsDifficulty = document.querySelectorAll('.difficulty');
@@ -9,18 +11,34 @@ const btnStart = document.querySelector('.settings__start');
 const gameEle = document.querySelector('.game');
 const questionEle = document.querySelector('.game__question');
 const answersEle = document.querySelector('.game__answers');
+const btnNext = document.querySelector('.game__next');
+const pageIndex = document.querySelector('.game__page');
+
+const scoreEle = document.querySelector('.score');
+const scoreMsgEle = document.querySelector('.score__message');
+
+const errorEle = document.querySelector('.error');
+const errorMsgEle = document.querySelector('.error__message');
+
+const btnAgain = document.querySelectorAll('.btn--again');
 
 class App {
   #categories = {};
   #settings = {};
   #questions = [];
   #index = 0;
+  #score = 0;
 
   constructor() {
     this.#loadCategories();
 
     // EVENT LISTENERS
     btnStart.addEventListener('click', this.#startGame.bind(this));
+    answersEle.addEventListener('click', this.#checkAnswer.bind(this));
+    btnNext.addEventListener('click', this.#nextQuestion.bind(this));
+    Array.from(btnAgain).forEach(btn => {
+      btn.addEventListener('click', this.#resetGame.bind(this));
+    });
   }
 
   async #loadCategories() {
@@ -33,15 +51,16 @@ class App {
       this.#categories = data.trivia_categories;
       console.log(this.#categories);
       this.#renderCategories();
+      btnStart.disabled = false;
     } catch (err) {
-      console.error(err);
+      this.#renderError(err);
     }
   }
 
   #renderCategories() {
     // MARKUP FOR CATEGORY SELECT FORM
     settingsCategories.innerHTML = `
-      <option value="">Any Category</option>
+      <option selected value="">Any Category</option>
     `;
     this.#categories.forEach(cat =>
       settingsCategories.insertAdjacentHTML(
@@ -80,14 +99,10 @@ class App {
       console.log(this.#settings);
       //GET QUESTIONS FROM API
       await this.#loadQuestions();
-      //RENDER THE FIRST QUESTION AND ANSWERS
-      this.#renderQuestions();
-      this.#renderAsnwers();
-      //HIDE SETTINGS AND DISPLAY GAME
-      settingsEle.classList.add('hidden');
-      gameEle.classList.remove('hidden');
+      //RENDER THE GAME
+      this.#renderGame();
     } catch (err) {
-      console.error(err);
+      this.#renderError(err);
     }
   }
 
@@ -107,14 +122,23 @@ class App {
         throw new Error(`No questions for this topic :(`);
       // SAVE QUESTIONS
       this.#questions = data.results;
+      console.log(this.#questions);
     } catch (err) {
-      console.error(err);
+      this.#renderError(err);
     }
+  }
+
+  #renderGame() {
+    this.#renderQuestions();
+    this.#renderAsnwers();
+    this.#renderPageIndex();
+    //HIDE SETTINGS AND DISPLAY GAME
+    settingsEle.classList.add('hidden');
+    gameEle.classList.remove('hidden');
   }
 
   #renderQuestions() {
     if (!this.#questions.length) return;
-    console.log('works');
     //DISPLAY THE CURRENT QUESTION
     questionEle.innerHTML = this.#questions[this.#index].question;
   }
@@ -143,14 +167,108 @@ class App {
       answersEle.insertAdjacentHTML(
         'beforeend',
         `
-        <div class="game__answer">${answersShuffled[i]}</div>
+        <button class="game__answer">${answersShuffled[i]}</button>
         `
       );
     }
   }
 
+  #renderPageIndex() {
+    pageIndex.innerHTML = `
+      ${this.#index + 1}/${this.#questions.length}
+    `;
+  }
+
+  #checkAnswer(e) {
+    const target = e.target;
+    if (!target.classList.contains('game__answer')) return;
+    //GET THE USERS ANSWER
+    const answer = target.innerHTML;
+    //CHECK IF IT IS CORRECT
+    if (answer === this.#questions[this.#index].correct_answer)
+      //IF YES, DISPLAY SUCCES
+      this.#correctAnswer(target);
+    //IF NOT, DISPLAY MISTAKE
+    else this.#wrongAnswer(target);
+    //SHOW CORRECT ANSWER
+    this.#showCorrect();
+    //ENABLE NEXT BUTTON
+    btnNext.disabled = false;
+  }
+
+  #correctAnswer(element) {
+    //INCREASE SCORE FOR CORRECT ANSWER
+    this.#score++;
+    //DISPLAY DESIRED STYLES
+    element.classList.add('correct');
+    container.classList.add('correct');
+    //DISABLE ANSWERS
+    this.#disableAnswers(true);
+  }
+
+  #wrongAnswer(element) {
+    //DISPLAY DESIRED STYLES
+    element.classList.add('wrong');
+    container.classList.add('wrong');
+    //DISABLE ANSWERS
+    this.#disableAnswers(true);
+  }
+
+  #disableAnswers(boolean) {
+    Array.from(answersEle.children).forEach(ele => (ele.disabled = boolean));
+  }
+
+  #showCorrect() {
+    Array.from(answersEle.children).forEach(ele => {
+      if (ele.innerHTML === this.#questions[this.#index].correct_answer)
+        ele.classList.add('correct');
+    });
+  }
+
+  #nextQuestion() {
+    container.classList.remove('correct');
+    container.classList.remove('wrong');
+    if (this.#index === this.#questions.length - 1) this.#renderScore();
+    else {
+      this.#index++;
+      this.#renderGame();
+      btnNext.disabled = true;
+    }
+  }
+
+  #renderScore() {
+    scoreMsgEle.innerHTML = `Correct answers: ${this.#score} of ${
+      this.#questions.length
+    }`;
+    settingsEle.classList.add('hidden');
+    gameEle.classList.add('hidden');
+    scoreEle.classList.remove('hidden');
+  }
+
   #renderSpinner(btn) {
     btn.innerHTML = `<i class="fas fa-cog"></i>`;
+  }
+
+  #renderError(err) {
+    errorMsgEle.innerHTML = `${err}`;
+    errorEle.classList.remove('hidden');
+  }
+
+  #resetGame() {
+    console.log('reset');
+    this.#score = 0;
+    this.#index = 0;
+    this.#settings = {};
+    this.#questions = [];
+    questionEle.innerHTML = '';
+    answersEle.innerHTML = '';
+    pageIndex.innerHTML = '';
+    btnStart.innerHTML = 'Start';
+    settingsEle.reset();
+    gameEle.classList.add('hidden');
+    settingsEle.classList.remove('hidden');
+    errorEle.classList.add('hidden');
+    scoreEle.classList.add('hidden');
   }
 }
 
